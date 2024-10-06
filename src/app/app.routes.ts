c70e1d53db07279848b3f616d51e06121f10eee6
Omar Amgad <103358321+OmarAmgad9@users.noreply.github.com>
2024-10-06 17:32:48 +0300

    starting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainComponentComponent } from './main-component/main-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'/profile', title:'profile',  component: MainComponentComponent },
+
+];
